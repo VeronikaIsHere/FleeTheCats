278e557c17492941a0aaea0ebc4ef76ae5095084
VeronikaIsHere <veronika.frei2002@gmail.com>
2023-03-14 10:07:11 +0100

    eyetracker for cats

--- catgame/Assets/fleethecat/Scripts/FollowPlayer.cs
+++ catgame/Assets/fleethecat/Scripts/FollowPlayer.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
+using Tobii.Gaming;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -11,26 +12,38 @@ public class FollowPlayer : MonoBehaviour
     public Transform target;  // player
     public int inRangeDistance;  // when the cat should notice the player
     NavMeshAgent nav;
-   
+    private GazeAware _gazeAware;
+    private int catCooldown = 0;
+
 
     // Start is called before the first frame update
     void Start()
     {
         nav = GetComponent<NavMeshAgent>();
-        
+        _gazeAware = GetComponent<GazeAware>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance (transform.position, target.transform.position) < inRangeDistance)  // check distance
+        if (catCooldown-- <= 0 && Vector3.Distance(transform.position, target.transform.position) < inRangeDistance) // check cooldown and distance
         {
-            nav.SetDestination(target.position);  // go to position of player 
-        } 
+            if (_gazeAware.HasGazeFocus) //check if gaze on cat
+            {
+                catCooldown = 100;
+                nav.destination = transform.position; //dont move
+            }
+            else
+            {
+                nav.SetDestination(target.position);  // go to position of player
+            }
+        }
         else
         {
             nav.destination = transform.position;  // stop following 
         }
+
         
+
     }
 }
