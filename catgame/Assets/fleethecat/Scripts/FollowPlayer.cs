de8a387a2e6bb655028b81d99ba8e743c70a7b81
VeronikaIsHere <veronika.frei2002@gmail.com>
2023-03-14 10:32:25 +0100

    max cooldown added

--- catgame/Assets/fleethecat/Scripts/FollowPlayer.cs
+++ catgame/Assets/fleethecat/Scripts/FollowPlayer.cs
@@ -12,9 +12,9 @@ public class FollowPlayer : MonoBehaviour
     public Transform target;  // player
     public int inRangeDistance;  // when the cat should notice the player
     NavMeshAgent nav;
-    private GazeAware _gazeAware;
-    private int catCooldown = 0;
-
+    private GazeAware _gazeAware; // use the gaze
+    private int catCooldown = 100; // max cooldown for cats (can be made public or serialized if prefeb)
+    private int currentCooldown = 0; // currrent cooldown (can be < 0
 
     // Start is called before the first frame update
     void Start()
@@ -26,11 +26,11 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (catCooldown-- <= 0 && Vector3.Distance(transform.position, target.transform.position) < inRangeDistance) // check cooldown and distance
+        if (currentCooldown-- <= 0 && Vector3.Distance(transform.position, target.transform.position) < inRangeDistance) // check cooldown and distance
         {
             if (_gazeAware.HasGazeFocus) //check if gaze on cat
             {
-                catCooldown = 100;
+                currentCooldown = catCooldown;
                 nav.destination = transform.position; //dont move
             }
             else
